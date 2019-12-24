4a1b406e148618a4ab23866187309e95d3ebbef6
UKMattM <matthewmoss98@gmail.com>
2019-12-24 03:28:28 +0000

    Turned project in to a bot. Created embeds, created mapurl thumbnails.

--- bot.py
+++ bot.py
@@ -1,34 +1,70 @@
-#!/usr/bin/env python3
-
+import asyncio
+import discord
+import config
 from steam import SteamQuery
+from discord.ext import commands
+
+bot = commands.Bot(command_prefix=config.BOT_CMD_PREFIX)
+
+
+@bot.event
+async def on_ready():
+    print('Logged in as')
+    print(bot.user.name)
+    print(bot.user.id)
+    print('------')
+    await joinkit()
+
 
 HOST = "167.88.11.228"
-PORT = 27165
+PORT1 = 27165
+PORT2 = 27175
+PORT3 = 27195
 
-server_obj = SteamQuery(HOST, PORT)
 
-# This will store the last results so you dont need to query again
-#return_dictionary = server_obj.return_last_data()
+async def joinkit():
+    n = 0
+    while n != 3:
+        if n == 0:
+            server_obj = SteamQuery(HOST, PORT1)
+            quicklink = f"{HOST}:27166"
+        if n == 1:
+            server_obj = SteamQuery(HOST, PORT2)
+            quicklink = f"{HOST}:27176"
+        if n == 2:
+            server_obj = SteamQuery(HOST, PORT3)
+            quicklink = f"{HOST}:27186"
+        n = n + 1
+        server_info = server_obj.query_game_server()
+        await embedmaker(server_info, quicklink)
+    if n == 3:
+        await sleeptimer()
 
-# New results, also saved and can be retrieved via the return_last_data method
-return_dictionary = server_obj.query_game_server()
 
-#return_dictionary {
-#   'online': True,
-#   'ip': 'ip',
-#   'port': port,
-#   'name': 'name',
-#   'map': 'map',
-#   'game': 'game',
-#   'description': 'server desc',
-#   'players': players,
-#   'max_players': slots,
-#   'bots': bots,
-#   'password_required': bool,
-#   'vac_secure': bool,
-#   'server-type': 'type',
-#   'os': 'os'
-#   }
+async def embedmaker(server_info, quicklink):
+    channel = bot.get_channel(config.SERVER_CHANNEL)
+    if server_info['map'] == "CAF_Yehorivka_TC_V1":
+        mapurl = "Yehorivka_TC_v1"
+    else:
+        mapurl = server_info['map'].replace(" ", "_")
+    embed = discord.Embed(
+        title=server_info['name']
+    )
+    embed.set_thumbnail(url=f"https://squadmaps.com/full/{mapurl}.jpg")
+    if server_info['players'] > server_info['max_players']:
+        players = server_info['players'] - server_info['max_players']
+        embed.add_field(name='Played Count', value=f"{server_info['max_players']}/{server_info['max_players']} "
+                                                   f"(+{players})")
+    else:
+        embed.add_field(name='Played Count', value=f"{server_info['players']}/{server_info['max_players']}")
+    embed.add_field(name='Map', value=f"{server_info['map']}", inline=True)
+    embed.add_field(name='Quick Connect', value=f"steam://connect/{quicklink}", inline=False)
+    await channel.send(embed=embed)
 
-print(return_dictionary)
 
+async def sleeptimer():
+    await asyncio.sleep(60)
+    await joinkit()
+
+if __name__ == "__main__":
+    bot.run(config.BOT_TOKEN)
